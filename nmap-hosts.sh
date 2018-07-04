29c80b73fa930c81d1e3af35148c211a1f083379
global4g <global4glv@gmail.com>
2018-07-03 17:26:31 -0700

    Create nmap-hosts.sh
    Generate a list of hosts based on IP Range / CIDR

---
+++ nmap-hosts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Usage ./nmap-hosts.sh 10.10.10.1/23
+#       Generate a list of hosts based on IP Range
+#       Creates a file hosts-timestamp.txt
+
+# -sn was known as -sP in previous releases
+
+FILE=hosts-$(date +%s).txt
+sudo nmap -sn "$1"  -Pn > $FILE 
+grep -o '.*)$' $FILE | grep -o -P "(?<=scan report for ).*" | awk '{print $1, $2}'
